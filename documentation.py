a3ffda188e4c63f7b05216ce2af9e1efa60f8f9b
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2020-02-03 10:42:42 -0500

    Create documentation.py

---
+++ documentation.py
@@ -0,0 +1,13 @@
+#-----------------------------------------------------------------------------
+# Name:        documentation.py
+# Purpose:     To provide an exemplar of documentation stantards in Python. 
+#
+# References: 	This script uses the PEP-8 style guide
+#				here: https://www.python.org/dev/peps/pep-0008/
+#
+# Author:      Haider Zaidi
+# Created:     03-Feb-2020
+# Updated:     
+#-----------------------------------------------------------------------------
+
+def menu():
