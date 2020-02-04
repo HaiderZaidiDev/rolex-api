6d4cf726e16a21479e91cb671bf30c175fbf5f09
Haider Zaidi <45272003+HaiderZaidiDev@users.noreply.github.com>
2020-02-04 14:33:24 -0500

    Update documentation.py

--- documentation.py
+++ documentation.py
@@ -10,8 +10,56 @@
 # Updated:     
 #-----------------------------------------------------------------------------
 
+## Watches 
+import os
+
+def error(errorMessage):
+  print("Error: {}.".format(str(errorMessage)))
+  os.system('cls')
+
+def menu(username):
+  print("Welcome, {].".format(username))
+  try:
+    optionSelect = int(input('Please select an option below: '))
+  except valueError:
+    error(Only integer values are accepted, please try again)
+    menu()
+    
+  print("""
+  1. Process Transaction \n
+  2. Inventory Management \n
+  3. Sales Reports
+  """)
+  
+  if optionSelect = 1:
+    transactions()
+  elif optionSelect = 2:
+    inventory()
+  elif optionSelect = 3:
+    reports()
+  else:
+    error(Invalid option selected, please try again)
+    menu()
+ 
+  
 def login():
   username = input('Username: ')
   password = input('Password: ')
   hash = '{}:{}'.format(username, password) # Format for verificiation.
-
+  failedAttempts = 0
+  userData = open('userdata.txt', 'r')
+  userDataContent = userData.read()
+  userdata.close()
+  
+  if hash in userDataContent:
+    menu(username)
+  
+  else:
+    failedAttempts +=1
+    attemptsRemaining = 3 - failedAttempts
+    if failedAttempts = 3:
+      error(Access denied, 
+     exit()
+login()
+     
+    
